ed31f76ec3142e1accded37b144e3d7d05136c3f
YING, JACKY <sy4593@exo.att.com>
2024-10-30 16:30:43 -0700

    update authnrequest model

--- authn_request.go
+++ authn_request.go
@@ -18,18 +18,17 @@ import "time"
 
 // AuthNRequest is the go struct representation of an authentication request
 type AuthNRequest struct {
-	ID                          string `xml:",attr"`
-	Version                     string `xml:",attr"`
-	ProtocolBinding             string `xml:",attr"`
-	AssertionConsumerServiceURL string `xml:",attr"`
+	ID                          string    `xml:",attr"`
+	Version                     string    `xml:",attr"`
+	ProtocolBinding             string    `xml:",attr"`
+	AssertionConsumerServiceURL string    `xml:",attr"`
+	IssueInstant                time.Time `xml:",attr"`
+	Destination                 string    `xml:",attr"`
 
-	IssueInstant time.Time `xml:",attr"`
-
-	Destination string `xml:",attr"`
-	Issuer      string
+	Issuer Issuer
 }
 
 type Issuer struct {
-	SPProvidedID string `xml:",SPProvidedID"`
+	SPProvidedID string `xml:",attr"`
 	Issuer       string
 }
